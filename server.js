0c4750899408b1618e44b2c9f75611839698cf48
abelmaniquis <abelmaniquis@gmail.com>
2016-07-15 22:48:05 +0000

    testing

--- server.js
+++ server.js
@@ -5,25 +5,50 @@ var unirest = require('unirest');
 var express = require('express');
 var events = require('events');
 
+//Calls the Spotify API
+
+/*var test = unirest.get("https://api.spotify.com/v1/artists/2aaLAng2L2aWD2FClzwiep/related-artists");
+console.log(test.qs());
+*/
 var getFromApi = function(endpoint, args) {
     var emitter = new events.EventEmitter();
-    unirest.get('https://api.spotify.com/v1/' + endpoint)
-           .qs(args)
+    var endpoint = "artists/2aaLAng2L2aWD2FClzwiep/related-artists"; //test
+    unirest.get('https://api.spotify.com/v1/' + endpoint) 
+           .qs(args)//This queries the string
            .end(function(response) {
                 if (response.ok) {
                     emitter.emit('end', response.body);
+                    console.log(emitter);
                 }
                 else {
                     emitter.emit('error', response.code);
                 }
+            /*Here, we make a request to the get related artists endpoint.
+              If the request is succesful, then artist.related should be set to item.artists
+              where item is the object returned by the get related artists endpoint.
+            */
             });
     return emitter;
 };
 
-var app = express();
-app.use(express.static('public'));
 
+/*
+When a user makes a request to /search/:name, you are going to make a
+request to the Spotify/search endpoint to find information on the artist
+which they are looking for.
+*/
+var app = express();
+app.use(express.static('public'));      //node.static calls the front end.
+/*
+When a request to /search/:name is made, 
+
+getFromAPI uses the following endpoint:
+/search?q=<name>&limit=1&type=artist.
+
+You then add listeners to the EventEmitter
+*/
 app.get('/search/:name', function(req, res) {
+    
     var searchReq = getFromApi('search', {
         q: req.params.name,
         limit: 1,
@@ -31,9 +56,8 @@ app.get('/search/:name', function(req, res) {
     });
 
     searchReq.on('end', function(item) {
-        var artist = item.artists.items[0];
+        var artist = item.artists/*.items[0]*/;
         res.json(artist);
-        console.log(artist);
     });
 
     searchReq.on('error', function(code) {
